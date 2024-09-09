f8a9f7ccada1a993bc356295134d04f51d2afb1b
jojayeon <jojayeon6152@gmail.com>
2024-09-09 14:33:09 +0900

    .py input

---
+++ jojayeon2/inout.py
@@ -0,0 +1,26 @@
+# my_name1 = input("이름의 성을 줄래:")
+# my_name2 = input("성을 제외한 이름을 적어줄래:")
+
+# def print_name(name, name2):
+#     print("이름 : " + f"{name + name2}")
+    
+# print_name(my_name1,my_name2)
+
+# my_name = input("이름:")
+
+# def print_name(name):
+#     print("이름 : " + f"{name}")
+    
+# print_name(my_name[0])
+# print_name(my_name)
+
+
+a = input("이름:")
+
+def print_name(A):
+    print(A)
+    
+print_name(a)
+
+
+
